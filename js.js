c13c065ceb95d9ebb0db51f678a3c66629a2c20b
Alejo Vera Laffratta <alejoveralaffratta21@gmail.com>
2024-11-17 15:45:26 -0300

    Dia 19

---
+++ js.js
@@ -0,0 +1,6 @@
+<script>
+    const button = document.querySelector('.header__button');
+    button.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+    });
+</script>
