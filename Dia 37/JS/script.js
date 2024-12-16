73ad731950c85ee96de84b9d3bdd4e761288e03d
AlejoVera97 <alejoveralaffratta21@gmail.com>
2024-12-16 10:46:38 -0300

    dia 37

---
+++ Dia 37/JS/script.js
@@ -0,0 +1,63 @@
+//1- Constantes y variables.
+const sliderImages = document.querySelector('.Slider-images');
+const images = document.querySelectorAll('.Slider-img');
+const btnNext = document.querySelector('#btnNext');
+const btnPrev = document.querySelector('#btnPrev');
+const spanActual = document.getElementById('actual');
+const spanTotal = document.getElementById('total');
+let currentImageIndex = 0;
+const totalImages = images.length;
+
+
+
+//2- EventListeners
+btnNext.addEventListener('click', nextImage);
+btnPrev.addEventListener('click', prevImage);
+
+
+//3- Ejecucion de codigo
+function nextImage() {
+    currentImageIndex++;
+    if (currentImageIndex >= images.length) {
+        currentImageIndex = 0;
+    }
+    actualizarSlider();
+    actualizarContador();
+}
+function prevImage() {
+    currentImageIndex--;
+    if (currentImageIndex < 0) {
+        currentImageIndex = images.length - 1;
+    }
+    actualizarSlider();
+    actualizarContador();
+}
+function actualizarSlider() {
+    const width = 400;
+    sliderImages.style.transform = `translateX(${-width * currentImageIndex}px)`;
+}
+function actualizarContador() {
+    spanActual.textContent = currentImageIndex + 1;
+}
+
+
+
+spanTotal.textContent = totalImages;
+actualizarContador();
+agregarIntervalo();
+
+
+// 4. Funcionalidad de intervalo
+
+sliderImages.addEventListener('mouseover', () => {
+    clearInterval(intervalos);
+});
+sliderImages.addEventListener('mouseout', () => {
+    agregarIntervalo();
+});
+function agregarIntervalo() {
+    intervalos = setInterval(nextImage, 1000);
+}
+
+
+    
